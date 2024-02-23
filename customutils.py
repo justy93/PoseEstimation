f58fa12997bad6800ec37e5a3362a57c20b810ed
justy93 <justine.scicluna@gmail.com>
2024-02-23 22:07:51 +0100

    Latest working + initial standard dev changes

--- customutils.py
+++ customutils.py
@@ -6,6 +6,7 @@ from decimal import Decimal, ROUND_HALF_UP
 
 #KEYPOINT_CONSTANTS = [float(x) for x in '0.026,0.025,0.025,0.035,0.035,0.079,0.079,0.072,0.072,0.062,0.062,0.107,0.107,0.087,0.087,0.089,0.089,0.026,0.026,0.107,0.025,0.025,0.025,0.025,0.025,0.025'.split(',')]
 KEYPOINT_CONSTANTS = [float(x) for x in '0.026,0.025,0.025,0.035,0.035,0.079,0.079,0.072,0.072,0.062,0.062,0.107,0.107,0.087,0.087,0.089,0.089,0.026,0.026,0.107,0.089,0.089,0.089,0.089,0.089,0.089'.split(',')]
+KEYPOINTS = ['nose', 'left_eye', 'right_eye', 'left_ear', 'right_ear', 'left_shoulder', 'right_shoulder', 'left_elbow', 'right_elbow', 'left_wrist', 'right_wrist', 'left_hip', 'right_hip', 'left_knee', 'right_knee', 'left_ankle', 'right_ankle', 'head', 'neck', 'hip', 'left_big_toe', 'right_big_toe', 'left_small_toe', 'right_small_toe', 'left_heel', 'right_heel']
 
 def get_classes(classes_path):
     with open(classes_path) as f:
@@ -83,6 +84,18 @@ def compute_area_keypoints(kpts):
     box_width = max_x - min_x
     box_height = max_y - min_y
     return box_width*box_height
+    
+
+def compute_area_keypoints(x_values, y_values):    
+    # Get the minimum x and y values
+    min_x = min(x_values)
+    max_x = max(x_values)
+    min_y = min(y_values)
+    max_y = max(y_values)
+
+    box_width = max_x - min_x
+    box_height = max_y - min_y
+    return box_width*box_height
 
 
 def load_images_dataframe():
@@ -139,11 +152,15 @@ def compute_oks(joint, d, area, visibility):
 
 
 def edit_keypoints(kpts):
-    kpts = np.array(kpts).reshape(-1,3)
+    kpts = np.array(kpts).reshape(-1,3).astype(float)
     vi = kpts[:,2]
     kpts = kpts[:,0:2]
     return kpts, vi
 
+
+def get_keypoint(kpts, id):
+    index = KEYPOINTS.index(id)
+    return [kpts[index*3],kpts[(index*3)+1]]
 
 def OKS(kpts1, kpts2, area, start, end):
 
